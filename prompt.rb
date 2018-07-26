3025aead7f60c9971f9d2a5efeeb47e19f7ff02f
alana-m-552 <alana.mackey0552@gmail.com>
2018-07-26 14:03:25 +0000

    automatically backed up by learn

--- prompt.rb
+++ prompt.rb
@@ -1,3 +1,3 @@
 price_of_dinner = puts "How much was the dinner?"
 price_of_dinner = gets.chomp.to_f
-return "You should tip $" + 0.22+price_of_dinner
+puts "You should tip $" + 0.22+price_of_dinner
